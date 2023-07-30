f0a7f2e31732452d7a6df8d58aaa7f2fe03068dc
NAO5802 <i100185b@gmail.com>
2023-07-30 16:00:00 +0900

    text styling sample

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
 
 class TextExample extends StatelessWidget {
-  static final bold24Roboto = TextStyle(
-    fontFamily: "Georgia",
-    fontSize: 24,
-    fontWeight: FontWeight.bold,
-  );
-
-  final childContainer = Center(
-    child: Container(
-      width: 240,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.red[400],
-      ),
-      child: Text(
-        'hello world',
-        style: bold24Roboto,
-        textAlign: TextAlign.center,
-      ),
-    ),
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +13,59 @@ class TextExample extends StatelessWidget {
         ),
         child: Transform(
           alignment: Alignment.center,
-          transform: Matrix4.identity()..scale(2.0),
-          child: childContainer,
+          transform: Matrix4.identity()..scale(1.5),
+          child: _getRedContainer(),
         )
+    );
+  }
+
+  TextStyle _getTextStyle() {
+    return TextStyle(
+      fontFamily: "Georgia",
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 8,
+    );
+  }
+
+  RichText _getRichText(){
+    return RichText(
+      text: TextSpan(
+        style: _getTextStyle(),
+        children: const <TextSpan>[
+          TextSpan(text: 'hello'),
+          TextSpan(
+              text: 'world',
+             style: TextStyle(
+               fontWeight: FontWeight.w300,
+               fontStyle: FontStyle.italic,
+               fontSize: 48,
+             )
+          )
+        ]
+      )
+    );
+  }
+
+  Center _getRedContainer() {
+    return Center(
+      child: Container(
+        width: 160,
+        // height: 160,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.red[400],
+          // shape: BoxShape.circle,
+          // borderRadius: const BorderRadius.all(Radius.circular(8)),
+        ),
+        child: Text(
+          'hello world Lorem ipsum dolor sit amet, consec etur',
+          style: _getTextStyle(),
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+        ),
+      ),
     );
   }
 }

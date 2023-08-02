7602fac006e94f122683e7579de2ba7532c6af17
NAO5802 <i100185b@gmail.com>
2023-08-02 18:41:24 +0900

    add patrol widget test(custom finder)

--- test/patrol_widget_test.dart
+++ test/patrol_widget_test.dart
@@ -1,14 +1,26 @@
+import 'package:flutter_sandbox/main.dart';
 import 'package:flutter_sandbox/my_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 
-void main () {
+void main() {
   // flutter test で実行できる
   patrolTest('counter is increment when plus button taped',
-          (PatrolTester $) async {
-            await $.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+      config: PatrolTesterConfig(visibleTimeout:  Duration(seconds: 10)),
+      (PatrolTester $) async {
+    await $.pumpWidget(const MyWidget(title: 'T', message: 'M'));
 
-            expect($('T'), findsOneWidget);
-            expect($('M'), findsOneWidget);
-          });
+    expect($('T'), findsOneWidget);
+    expect($('M'), findsOneWidget);
+  });
+
+  patrolTest("tap like", (PatrolTester $) async {
+    await $.pumpWidgetAndSettle(const MyApp());
+
+    await $(#like_button).tap();
+    await $(#favorite_menu_icon).tap();
+
+    // expect($('No favorites yet'), findsNothing); //  表示したか？ではなくウィジェットツリーに存在するか？をみてる
+    expect($('No favorites yet').visible, equals(false));
+  });
 }

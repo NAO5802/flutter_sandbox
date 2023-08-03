d7817989f2720658a89e43fcf74fa5ebea4de496
NAO5802 <i100185b@gmail.com>
2023-08-03 19:08:38 +0900

    add native integration test sample

--- integration_test/patrol_example_test.dart
+++ integration_test/patrol_example_test.dart
@@ -1,30 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 
 void main() {
   // ネイティブ自動化機能を使用するためにpatrol testで実行
-  patrolTest(
-      'change dark mode',
-      nativeAutomation: true,
-          ($) async {
+  patrolTest('back home and open app', nativeAutomation: true, ($) async {
+    await $.pumpWidgetAndSettle(const MyApp());
 
-        await $.pumpWidgetAndSettle(MaterialApp(
-          theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
-          themeMode: ThemeMode.system,
-          home: Scaffold(
-            appBar: AppBar(title: const Text("app"))
-          ),
-        ));
+    expect($("Like"), findsOneWidget);
 
-        expect($("app"), findsOneWidget);
-        await $.pump(Duration(seconds: 3));
+    await $.native.pressHome();
+    await $.native.openApp();
 
-        await $.native.pressHome();
-        await $.native.enableDarkMode();
-        await $.native.openApp();
-
-        expect($("app"), findsOneWidget);
-      });
+    expect($("Like"), findsOneWidget);
+  });
 }

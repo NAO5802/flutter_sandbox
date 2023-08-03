044615a256e54c4d213da0885a2965517ecbb3a2
NAO5802 <i100185b@gmail.com>
2023-08-03 21:48:44 +0900

    add widget test sample

--- test/patrol_widget_test.dart
+++ test/patrol_widget_test.dart
@@ -1,26 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_sandbox/main.dart';
-import 'package:flutter_sandbox/my_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 
 void main() {
   // flutter test で実行できる
-  patrolTest('counter is increment when plus button taped',
-      config: PatrolTesterConfig(visibleTimeout:  Duration(seconds: 10)),
-      (PatrolTester $) async {
-    await $.pumpWidget(const MyWidget(title: 'T', message: 'M'));
-
-    expect($('T'), findsOneWidget);
-    expect($('M'), findsOneWidget);
-  });
-
-  patrolTest("tap like", (PatrolTester $) async {
+  patrolTest("patrol custom finder", (PatrolTester $) async {
     await $.pumpWidgetAndSettle(const MyApp());
 
     await $(#like_button).tap();
     await $(#favorite_menu_icon).tap();
 
-    // expect($('No favorites yet'), findsNothing); //  表示したか？ではなくウィジェットツリーに存在するか？をみてる
     expect($('No favorites yet').visible, equals(false));
   });
+
+  testWidgets('flutter test', (widgetTester) async {
+    await widgetTester.pumpWidget(const MyApp());
+
+    await widgetTester.tap(find.byKey(Key('like_button')));
+    await widgetTester.tap(find.byKey(Key('favorite_menu_icon')));
+
+    expect(find.text("No favorites yet"), findsNothing);
+  });
+
+  patrolTest('mixed', (PatrolTester $) async{
+    await $.pumpWidgetAndSettle(const MyApp());
+
+    await $(#like_button).tap();
+
+    final WidgetTester widgetTester = $.tester;
+    await widgetTester.tap(find.byKey(Key('favorite_menu_icon')));
+
+    expect($('No favorites yet').visible, equals(false));
+  });
+
 }

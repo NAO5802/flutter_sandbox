d7fb4047b72923ac8fd45195352c49bd911eae4b
NAO5802 <i100185b@gmail.com>
2023-08-06 17:48:26 +0900

    build mock

---
+++ test/mock_test.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+
+// flutter pub run build_runner build でmock生成
+@GenerateMocks([http.Client])
+void main() {
+
+}

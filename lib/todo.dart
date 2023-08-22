077e5323fff05b5ea1102faae847457fd43f6f00
NAO5802 <i100185b@gmail.com>
2023-08-22 18:24:36 +0900

    add todo list view

---
+++ lib/todo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+final todos =
+List.generate(20, (index) => Todo('TODO $index', 'description of $index'));
+
+class TodoScreen extends StatelessWidget {
+  const TodoScreen({super.key, required this.todos});
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (content, index) {
+            return ListTile(title: Text(todos[index].title),);
+          },
+    ),);
+  }
+}

39090624499f58747dffdcf8bd91807b1a6c9b6f
NAO5802 <i100185b@gmail.com>
2023-08-27 23:00:21 +0900

    add catalog to main screen

--- lib/cart/models/cart.dart
+++ lib/cart/models/cart.dart
@@ -5,9 +5,15 @@ import 'package:flutter/cupertino.dart';
 import 'catalog.dart';
 
 class CartModel extends ChangeNotifier {
+  late CatalogModel _catalog;
   final List<Item> _items = [];
 
   UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
+  CatalogModel get catalog => _catalog;
+  set catalog(CatalogModel newCatalog){
+    _catalog = newCatalog;
+    notifyListeners();
+  }
 
   void add(Item item) {
     _items.add(item);
--- lib/cart/shopper_main.dart
+++ lib/cart/shopper_main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_sandbox/cart/models/cart.dart';
+import 'package:flutter_sandbox/cart/models/catalog.dart';
+import 'package:flutter_sandbox/cart/screens/catalog.dart';
+import 'package:provider/provider.dart';
 
 class MyShopper extends StatelessWidget {
   const MyShopper({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Text('my shopper');
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => CatalogModel()),
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+            create: (context) => CartModel(),
+            update: (context, catalog, cart) {
+              if (cart == null) throw ArgumentError.notNull('cart');
+              cart.catalog = catalog;
+              return cart;
+            })
+      ],
+      child: MyCatalog(),
+    );
   }
-  
 }

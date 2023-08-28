746c93ca0d324181bc7439460195c85ce57f0650
NAO5802 <i100185b@gmail.com>
2023-08-28 09:01:41 +0900

    WIP: add cart screen

--- lib/cart/screens/cart.dart
+++ lib/cart/screens/cart.dart
@@ -1,10 +1,43 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: _CartList(),
+                )
+            ),
+            const Divider(height: 4, color: Colors.black,),
+            _CartTotal()
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
+
+class _CartTotal extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
   }
-  
+}
+
+class _CartList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }

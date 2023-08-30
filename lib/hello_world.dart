13a6d3580738726fcbac5010507970eba0ff0fba
NAO5802 <i100185b@gmail.com>
2023-08-31 08:44:21 +0900

    WIP: add riverpod sample

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+final helloWorldProvider = Provider((_) => 'Hello world!');
+
 class MyHelloWorld extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
     return MaterialApp(
         home: Scaffold(
-      body: Text(
-        'hello world',
+      body: Center(
+        child: Text(
+          value,
+        ),
       ),
     ));
   }

1069906bd205170f1e329fef473fb097270a6fe3
NAO5802 <i100185b@gmail.com>
2023-09-03 23:18:59 +0900

    get ref from provider

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// Provider：変更されないオブジェクトを公開
+// 複数のプロバイダーを作成できる
+final helloWorldProvider = Provider((ref){
+ final repository = ref.watch(otherProvider);
 
-final helloWorldProvider = Provider((_) => 'Hello world!');
+ return '$repository!!!';
+});
+
+final otherProvider = Provider((ref) => 'Hello World');
 
 class MyHelloWorld extends ConsumerWidget {
   @override

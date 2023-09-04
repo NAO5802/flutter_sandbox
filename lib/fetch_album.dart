c88b3047c57a2702d93e110846f76a73e62d6b85
NAO5802 <i100185b@gmail.com>
2023-09-05 08:36:35 +0900

    add http request with header sample

--- lib/fetch_album.dart
+++ lib/fetch_album.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 Future<Album> fetchAlbum(http.Client client) async {
   final response = await client
-      .get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"));
+      .get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"),
+      headers: {
+        HttpHeaders.authorizationHeader: 'Basic your_api_token_here'
+      });
 
   if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
@@ -22,9 +26,9 @@ class Album {
 
   static Album fromJson(Map<String, dynamic> json) {
     return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title']
+        userId: json['userId'],
+        id: json['id'],
+        title: json['title']
     );
   }
 }

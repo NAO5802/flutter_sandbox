952d0b34aa05b2645e40865cc884a8103561cd31
NAO5802 <i100185b@gmail.com>
2023-09-05 08:43:47 +0900

    add http post request sample

--- lib/fetch_album.dart
+++ lib/fetch_album.dart
@@ -31,4 +31,22 @@ class Album {
         title: json['title']
     );
   }
+
+  Future<Album> createAlbum(String title) async{
+    final response = await http.post(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String> {
+        'title': title
+      })
+    );
+
+    if(response.statusCode == 201) {
+      return Album.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create album');
+    }
+  }
 }

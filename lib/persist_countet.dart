88acc14fbca3fe13e14204f78024d06adf0832eb
NAO5802 <i100185b@gmail.com>
2023-09-09 15:29:26 +0900

    add persist counter

--- lib/persist_countet.dart
+++ lib/persist_countet.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PersistCounter extends StatefulWidget {
   @override
@@ -9,7 +10,35 @@ class _PersistCounterState extends State<PersistCounter> {
   int _counter = 0;
 
   @override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('count: $_counter'));
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('count: $_counter'),
+            ElevatedButton(onPressed: _incrementCounter, child: Icon(Icons.add)),
+          ],
+    ));
+  }
+
+  Future<void> _loadCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0);
+    });
+  }
+
+  Future<void> _incrementCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _counter);
+    });
   }
 }
